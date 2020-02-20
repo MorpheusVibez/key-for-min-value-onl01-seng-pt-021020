fc483a8c67872b07b957d17968c60bb0eb8678cc
Andre Skeene <halodre24@gmail.com>
2020-02-20 20:24:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,8 @@ def key_for_min_value(name_hash)
   
   name_hash.collect do |name, value|
     if value <= 1
-      name
+      name << key_name
+      key_name[2]
     end
-    name[2] << key_name
-    key_name
   end
 end
